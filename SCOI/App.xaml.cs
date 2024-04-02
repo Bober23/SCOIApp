6bfa3c57611e1e18b16bd9ac94d0298033dca32b
PopovIO <i.o.popov@etos-pro.ru>
2024-04-02 14:41:51 +0300

    Добавьте файлы проекта.

---
+++ SCOI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SCOI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ SCOI/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SCOI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
